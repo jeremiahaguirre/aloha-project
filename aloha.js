3f04aed5cc58d6d4e449c306254af43d406a2b69
Jeremiah <happytobike@gmail.com>
2019-01-18 15:21:08 -0800

    smooth scoll and flikity wroking smoothy

--- aloha.js
+++ aloha.js
@@ -3,7 +3,35 @@ $("form").on("click", "button", function() {
   $(".botton").val("");
 });
 
+/* Smooth scrolling
+@souce https://css-tricks.com/smooth-scrolling-accessibility/
+
+*/
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1600
+        );
+        return false;
+      }
+    }
+  });
+});
+
 //Flickity PlugIn
+
 $(".carosel").flickity({
   // options
   cellAlign: "left",
