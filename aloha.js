0bba0087a0ea22fad17be31867ba569d7301391c
Jeremiah <happytobike@gmail.com>
2019-01-18 12:51:58 -0800

    Added Flikity plugIn

--- aloha.js
+++ aloha.js
@@ -2,3 +2,10 @@ $("form").on("click", "button", function() {
   alert("You have Subscribed");
   $(".botton").val("");
 });
+
+//Flickity PlugIn
+$(".carosel").flickity({
+  // options
+  cellAlign: "left",
+  contain: true
+});
