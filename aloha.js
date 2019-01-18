adaf65bc49bf4663f1b66bf665713fd9736e8053
Jeremiah <happytobike@gmail.com>
2019-01-18 14:39:52 -0800

    Smooth Scrolling in JS

--- aloha.js
+++ aloha.js
@@ -2,10 +2,38 @@ $("form").on("click", "button", function() {
   alert("You have Subscribed");
   $(".botton").val("");
 });
+/* Smooth scrolling
+@source https://css-tricks.com/smooth-scrolling-accessibility/
+
+*/
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1600
+        );
+        return false;
+      }
+    }
+  });
+});
 
 //Flickity PlugIn
 $(".carosel").flickity({
   // options
   cellAlign: "left",
-  contain: true
+  contain: true,
+  autoPlay: true,
+  prevNextButtons: false
 });
