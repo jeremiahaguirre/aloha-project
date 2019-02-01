8cdd4b143577e1e4b6eb84c6bb42388624f02719
Jeremiah <happytobike@gmail.com>
2019-01-31 16:27:40 -0800

    Added gulp and minifying of files, as well as browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,17 @@ const gulp = require("gulp"),
   uglifycss = require("gulp-uglifycss"),
   rename = require("gulp-rename"),
   concat = require("gulp-concat"),
-  streamqueue = require("streamqueue");
+  streamqueue = require("streamqueue"),
+  plumber = require("gulp-plumber"),
+  notify = require("gulp-notify"),
+  browserSync = require("browser-sync").create();
+
+let plumberErrorHandler = {
+  errorHandler: notify.onError({
+    title: "Gulp",
+    message: "Error: <%= error.message %>"
+  })
+};
 
 gulp.task("scripts", function() {
   return streamqueue(
@@ -12,6 +22,7 @@ gulp.task("scripts", function() {
     gulp.src("flickity.pkgd.min.js"),
     gulp.src("aloha.js")
   )
+    .pipe(plumber(plumberErrorHandler))
     .pipe(concat("all.js"))
     .pipe(terser()) // Call the terser function on these files
     .pipe(rename({ extname: ".min.js" })) // Rename the uglified file
@@ -19,12 +30,13 @@ gulp.task("scripts", function() {
 });
 
 gulp.task("css", function() {
-    return streamqueue(
-        { objectMode: true },
-        gulp.src("reset.css"),
-        gulp.src("aloha.css"),
-        gulp.src("flickity.min.css")
-        )
+  return streamqueue(
+    { objectMode: true },
+    gulp.src("reset.css"),
+    gulp.src("aloha.css"),
+    gulp.src("flickity.min.css")
+  )
+    .pipe(plumber(plumberErrorHandler))
     .pipe(concat("all.css"))
     .pipe(uglifycss()) // Call the  function on these files
     .pipe(rename({ extname: ".min.css" })) // Rename the uglified file
@@ -37,3 +49,19 @@ gulp.task("watch", function(done) {
 
   done();
 });
+
+//Browser-sync
+gulp.task("browser-sync", function(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "aloha.html"
+    }
+  });
+  gulp
+    .watch(["build/css/*.css", "build/js/*.js"])
+    .on("change", browserSync.reload);
+  done();
+});
+
+gulp.task("default", gulp.parallel("watch", "browser-sync"));
